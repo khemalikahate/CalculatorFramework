42860fe4daecc2e162941267f839f003e0f6b011
Khemali <khemalikahate@ayninfotech.com>
2019-12-03 12:15:55 +0530

    Initial Commit

---
+++ CalculatorFramework/CalculatorFramework.h
@@ -0,0 +1,19 @@
+//
+//  CalculatorFramework.h
+//  CalculatorFramework
+//
+//  Created by Apple on 03/12/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalculatorFramework.
+FOUNDATION_EXPORT double CalculatorFrameworkVersionNumber;
+
+//! Project version string for CalculatorFramework.
+FOUNDATION_EXPORT const unsigned char CalculatorFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalculatorFramework/PublicHeader.h>
+
+
